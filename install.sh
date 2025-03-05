fe6cf01390aad4451e7dbb81a7b32e8b9e946efc
Zander <CredibleOpossum@protonmail.com>
2025-03-05 00:27:47 -0500

    hopefully fix install script

--- install.sh
+++ install.sh
@@ -7,20 +7,17 @@ echo "You can easily change this in disko.nix"
 read -p "Are you sure you want to continue with the install? [y/N]: "
 echo
 if [[ "$REPLY" =~ ^[Yy]$ ]]; then
-    # Switch to root user, shouldn't require a prompt
-    sudo -i
-    
     # Partition device, default /dev/vda
-    nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ./disko-config.nix
+    sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko disko_config.nix
     
     # Generate config without filesystem as it was already done by disko.
-    nixos-generate-config --no-filesystems --root /mnt
+    sudo nixos-generate-config --no-filesystems --root /mnt
     
     # Move configs
     mv -f *.nix /mnt/etc/nixos
     
     # Install nixos
-    nixos-install --root /mnt --flake '/mnt/etc/nixos#nixos'
+    sudo nixos-install --root /mnt --flake '/mnt/etc/nixos#nixos'
     
     echo "Script ended without errors, feel free to reboot and test the installation."
 fi
